4436931c028e1beb8a35a0cd9e33c1ceca1e5438
mustabbas <42379733+mustabbas@users.noreply.github.com>
2021-08-31 01:56:54 +0300

    Fix

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+const humberger = document.getElementById('humberger');
+const closeButton = document.getElementById('closeButton');
+const portoflio = document.getElementById('portoflio');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
+
+const mobileNav = document.querySelector('.mobile-menu-wrap');
+const container = document.querySelector('.container');
+const container2 = document.querySelector('.footer-nav');
+const container3 = document.querySelector('.recent-work ');
+const container4 = document.querySelector('.card-container');
+
+function displayMenu() {
+  mobileNav.classList.add('display-none');
+  container.classList.remove('display-none');
+  container2.classList.remove('display-none');
+  container3.classList.remove('display-none');
+  container4.classList.remove('display-none');
+}
+
+function hideMenu() {
+  mobileNav.classList.remove('display-none');
+  container.classList.add('display-none');
+  container2.classList.add('display-none');
+  container3.classList.add('display-none');
+  container4.classList.add('display-none');
+  // window.location.hash = 'jump_to_this_location';
+}
+
+humberger.addEventListener('click', hideMenu);
+
+closeButton.addEventListener('click', displayMenu);
+
+portoflio.addEventListener('click', displayMenu);
+about.addEventListener('click', displayMenu);
+contact.addEventListener('click', displayMenu);
