29fddd2707176c6636abd3541e598e2e82ca7a23
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-08-30 23:08:41 +0300

    Add javascript to toggle menu

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
+let humberger = document.getElementById("humberger");
+let closeButton = document.getElementById("closeButton");
 
-let humberger = document.getElementById('humberger');
-humberger.addEventListener("click", function() {
-    console.log('here www');
-  });
+let mobileNav = document.querySelector(".mobile-menu-wrap");
+let container = document.querySelector(".container");
+let container2 = document.querySelector(".footer-nav");
+let container3 = document.querySelector(".recent-work ");
+let container4 = document.querySelector(".card-container");
+
+humberger.addEventListener("click", function () {
+  mobileNav.classList.remove("display-none");
+  container.classList.add("display-none");
+  container2.classList.add("display-none");
+  container3.classList.add("display-none");
+  container4.classList.add("display-none");
+});
+
+closeButton.addEventListener("click", function () {
+  mobileNav.classList.add("display-none");
+  container.classList.remove("display-none");
+  container2.classList.remove("display-none");
+  container3.classList.remove("display-none");
+  container4.classList.remove("display-none");
+});
