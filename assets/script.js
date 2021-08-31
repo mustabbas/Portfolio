b7ca1ad95735c42a0145362d208a450111020412
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-08-31 20:09:28 +0300

    Fix popup design for mobiel view and add javascript functionality to show and hide popup

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,50 @@
-const humberger = document.getElementById('humberger');
-const closeButton = document.getElementById('closeButton');
-const portoflio = document.getElementById('portoflio');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
+const humberger = document.getElementById("humberger");
+const closeButton = document.getElementById("closeButton");
+const portoflio = document.getElementById("portoflio");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
 
-const mobileNav = document.querySelector('.mobile-menu-wrap');
-const container = document.querySelector('.container');
-const container2 = document.querySelector('.footer-nav');
-const container3 = document.querySelector('.recent-work ');
-const container4 = document.querySelector('.card-container');
+const mobileNav = document.querySelector(".mobile-menu-wrap");
+const container = document.querySelector(".container");
+const container2 = document.querySelector(".footer-nav");
+const container3 = document.querySelector(".recent-work ");
+const container4 = document.querySelector(".card-container");
+const seeProjectButton = document.querySelectorAll(".see-project-button");
+const popUp = document.querySelector(".popup-wrap");
+const popUpCloseButton = document.querySelector("#popup-close-button");
+const bodyWrapper = document.querySelector("#body-wrapper");
 
 function displayMenu() {
-  mobileNav.classList.add('display-none');
-  container.classList.remove('display-none');
-  container2.classList.remove('display-none');
-  container3.classList.remove('display-none');
-  container4.classList.remove('display-none');
+  mobileNav.classList.add("display-none");
+  container.classList.remove("display-none");
+  container2.classList.remove("display-none");
+  container3.classList.remove("display-none");
+  container4.classList.remove("display-none");
 }
 
 function hideMenu() {
-  mobileNav.classList.remove('display-none');
-  container.classList.add('display-none');
-  container2.classList.add('display-none');
-  container3.classList.add('display-none');
-  container4.classList.add('display-none');
+  mobileNav.classList.remove("display-none");
+  container.classList.add("display-none");
+  container2.classList.add("display-none");
+  container3.classList.add("display-none");
+  container4.classList.add("display-none");
 }
 
-humberger.addEventListener('click', hideMenu);
+function togglePopup() {
+  popUp.classList.toggle("display-none");
+  bodyWrapper.classList.toggle("display-none");
+}
 
-closeButton.addEventListener('click', displayMenu);
+humberger.addEventListener("click", hideMenu);
 
-portoflio.addEventListener('click', displayMenu);
-about.addEventListener('click', displayMenu);
-contact.addEventListener('click', displayMenu);
+closeButton.addEventListener("click", displayMenu);
+
+portoflio.addEventListener("click", displayMenu);
+about.addEventListener("click", displayMenu);
+contact.addEventListener("click", displayMenu);
+
+seeProjectButton.forEach((element) => {
+  element.addEventListener("click", togglePopup);
+});
+
+popUpCloseButton.addEventListener("click", togglePopup);
