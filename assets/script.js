fbb8cb6f09d3db7d238833b1fbed026f5737f38a
mustabbas <42379733+mustabbas@users.noreply.github.com>
2021-09-01 02:25:21 +0300

    Add javaScript object and Fix style

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,66 @@
-const humberger = document.getElementById("humberger");
-const closeButton = document.getElementById("closeButton");
-const portoflio = document.getElementById("portoflio");
-const about = document.getElementById("about");
-const contact = document.getElementById("contact");
+const humberger = document.getElementById('humberger');
+const closeButton = document.getElementById('closeButton');
+const menuItem = document.querySelectorAll('#portoflio,#about,#contact');
+const mobileNav = document.querySelector('.mobile-menu-wrap');
+const seeProjectButton = document.querySelectorAll('.see-project-button');
+const popUp = document.querySelector('.popup-wrap');
+const popUpCloseButton = document.querySelector('#popup-close-button');
+const bodyWrapper = document.querySelector('#body-wrapper');
+const popupcardheader = document.querySelector('#popup-card-header');
+const popupCardImg = document.querySelector('#popup-card-img');
+const popupCardDescription = document.querySelector('#popup-card-description');
 
-const mobileNav = document.querySelector(".mobile-menu-wrap");
-const container = document.querySelector(".container");
-const container2 = document.querySelector(".footer-nav");
-const container3 = document.querySelector(".recent-work ");
-const container4 = document.querySelector(".card-container");
-const seeProjectButton = document.querySelectorAll(".see-project-button");
-const popUp = document.querySelector(".popup-wrap");
-const popUpCloseButton = document.querySelector("#popup-close-button");
-const bodyWrapper = document.querySelector("#body-wrapper");
+const objectcard1 = {
+  name: 'Multi-post story1',
+  image: './assets/img/WorkPopup.png',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+};
+const objectcard2 = {
+  name: 'Multi-post story2',
+  image: './assets/img/WorkPopup.png',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+};
+const objectcard3 = {
+  name: 'Multi-post story3',
+  image: './assets/img/WorkPopup.png',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+};
+const objectcard4 = {
+  name: 'Multi-post story4',
+  image: './assets/img/WorkPopup.png',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+};
+const objectcard5 = {
+  name: 'Multi-post story5',
+  image: './assets/img/WorkPopup.png',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+};
 
-function displayMenu() {
-  mobileNav.classList.add("display-none");
-  container.classList.remove("display-none");
-  container2.classList.remove("display-none");
-  container3.classList.remove("display-none");
-  container4.classList.remove("display-none");
+const objectCard = [objectcard1, objectcard2, objectcard3, objectcard4, objectcard5];
+
+function toggleMenu() {
+  mobileNav.classList.toggle('display-none');
+  bodyWrapper.classList.toggle('display-none');
 }
 
-function hideMenu() {
-  mobileNav.classList.remove("display-none");
-  container.classList.add("display-none");
-  container2.classList.add("display-none");
-  container3.classList.add("display-none");
-  container4.classList.add("display-none");
+function togglePopup(i) {
+  popUp.classList.toggle('display-none');
+  bodyWrapper.classList.toggle('display-none');
+  popupcardheader.textContent = objectCard[i].name;
+  popupCardImg.src = objectCard[i].image;
+  popupCardDescription.textContent = objectCard[i].description;
 }
 
-function togglePopup() {
-  popUp.classList.toggle("display-none");
-  bodyWrapper.classList.toggle("display-none");
-}
-
-humberger.addEventListener("click", hideMenu);
-
-closeButton.addEventListener("click", displayMenu);
-
-portoflio.addEventListener("click", displayMenu);
-about.addEventListener("click", displayMenu);
-contact.addEventListener("click", displayMenu);
-
-seeProjectButton.forEach((element) => {
-  element.addEventListener("click", togglePopup);
+humberger.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+menuItem.forEach((element) => {
+  element.addEventListener('click', toggleMenu);
 });
 
-popUpCloseButton.addEventListener("click", togglePopup);
+for (let i = 0; i < seeProjectButton.length; i + 1) {
+  seeProjectButton[i].addEventListener('click', () => {
+    togglePopup(i);
+  });
+}
+
+popUpCloseButton.addEventListener('click', togglePopup);
